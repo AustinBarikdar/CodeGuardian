2de75403d20d2de0cdc76f7f463f2eb112256543
AustinBarikdar <austinbarikdar@gmail.com>
2025-08-01 16:32:40 -0400

    Added gitignore file and middleware for signature

---
+++ src/middleware/verifySignature.js
@@ -0,0 +1,13 @@
+function verifySignature(req, res, next) {
+  const signature = req.headers['x-hub-signature-256'];
+  if (!signature) {
+    return res.status(401).send('No signature found');
+  }
+  const hmac = crypto.createHmac('sha256', GITHUB_SECRET);
+  const digest = 'sha256=' + hmac.update(req.rawBody).digest('hex');
+  if (signature !== digest) {
+    return res.status(401).send('Invalid signature');
+  }
+  next();
+}
+
